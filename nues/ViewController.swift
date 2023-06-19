71812e8789c51c580696d9210c8111d941b29f0a
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2023-06-19 13:56:32 +0700

    init

---
+++ nues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nues
+//
+//  Created by Sendo Tjiam on 19/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
